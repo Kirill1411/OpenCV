e50c169a78f38b4c9c8f49541175cfe35c1af2b2
KirillZotov <zyzywin@mail.ru>
2022-10-15 19:12:11 +0300

    Finish

---
+++ Video.py
@@ -0,0 +1,18 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 33, 123), 2)
+
+    cv2.imshow('Result', frame)
+
+    if cv2.waitKey(1) & 0xff == ord(' '):
+        break
